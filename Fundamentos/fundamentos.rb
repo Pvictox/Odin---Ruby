92bc633eaee775c8fb672b1c594d453dd831ce05
Pedro <paf.victor@gmail.com>
2023-03-02 10:11:58 -0300

    First Commit

---
+++ Fundamentos/fundamentos.rb
@@ -0,0 +1,11 @@
+print(6.even?);
+print("\n");
+print(6.odd?);
+print("\n");
+print("Welcome \n" + "To \n" + "Odin \n");
+
+name = "Pedro";
+
+print("Me chamo #{name}\n");
+
+print(:symbol.object_id == :symbol.object_id);
